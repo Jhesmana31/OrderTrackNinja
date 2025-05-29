3734c7723a9a5f00507e926b427eb14fadf9db0d
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-30 00:15:57 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-8import express, { type Request, Response, NextFunction } from "express";
+import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
