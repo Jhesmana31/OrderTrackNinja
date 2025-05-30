81aad02c2b449fd99cfd19d995c3ec9cf3c7ce45
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-30 09:18:31 +0800

    Update supabase.ts

--- server/supabase.ts
+++ server/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) throw new Error('supabaseUrl is required.');
+if (!supabaseKey) throw new Error('supabaseKey is required.');
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
